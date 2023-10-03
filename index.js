9adb9762504e5c1e0ec30be45a6a086f12971b08
linda wanjiku <lindashykooh@gmail.com>
2023-10-04 01:36:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// const input = document.getElementById('input');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
 function addingEventListener() {
+  const input = document.getElementById('input');
+
+  input.addEventListener('click', function() {
+    alert('I was clicked!');
+  });
 }
+
+// Call the function to bind the event listener
+addingEventListener();
